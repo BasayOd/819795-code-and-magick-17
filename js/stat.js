ad32ebfe7a0c4005da20a9c6b05228dab9a18254
Basay <Basay1980@gmail.com>
2019-06-12 13:59:48 +0300

    Сделал статистику с разными колонками через цикл

--- js/stat.js
+++ js/stat.js
@@ -4,17 +4,38 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
-var names = [ {'Кекс': 4025}, {'Катя': 1244}, {'Игорь': 1339}, {'Вы': 2725}];
+var TEXT_GAP = 20;
+var GISTO_HEIGTH = 150;
+var COLUMN_WIDTH = 40;
+var COLUMN_SPACE = 50;
+var max = 0;
+var columnHeight = GISTO_HEIGTH - GAP;
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
-window.renderStatistics = function (ctx, names) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
 
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   ctx.font = '16px PT Mono';
   ctx.fillStyle = 'black';
-  ctx.fillText('Ура Вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+  ctx.fillText('Ура вы победили!', CLOUD_X + TEXT_GAP, TEXT_GAP * 2);
+  ctx.fillText('Список результатов:', CLOUD_X + TEXT_GAP, TEXT_GAP * 3);
+  for (var i = 0; i < names.length; i++) {
+    if (times[i] > max) {
+      max = times[i];
+    }
+  }
+  for (i = 0; i < names.length; i++) {
+    ctx.fillText(Math.floor(times[i]), CLOUD_X + 2 * TEXT_GAP + i * (COLUMN_SPACE + COLUMN_WIDTH), CLOUD_HEIGHT - 4 * GAP - times[i] / max * columnHeight);
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+    }
+    ctx.fillRect(CLOUD_X + 2 * TEXT_GAP + i * (COLUMN_SPACE + COLUMN_WIDTH), CLOUD_HEIGHT - 3 * GAP - times[i] / max * columnHeight, COLUMN_WIDTH, times[i] / max * columnHeight);
+    ctx.fillStyle = 'black';
+    ctx.fillText(names[i], CLOUD_X + 2 * TEXT_GAP + i * (COLUMN_SPACE + COLUMN_WIDTH), CLOUD_HEIGHT - GAP);
+  }
 };
