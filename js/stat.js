6af3edb50b6d49ee41bf1d6bb1ebf8ada441521c
Basay <Basay1980@gmail.com>
2019-06-16 13:04:00 +0300

    сделал вызов функций удобночитаемыми

--- js/stat.js
+++ js/stat.js
@@ -35,25 +35,42 @@ var getColor = function (r, g, b, a) {
   return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + a + ')';
 };
 var renderSingleBar = function (ctx, x, y, width, height, names, times, maxTime, i) {
-  renderText(ctx, Math.floor(times[i]), CLOUD_X + 2 * TEXT_GAP + i * (COLUMN_SPACE + COLUMN_WIDTH),
-      CLOUD_HEIGHT - 4 * GAP - times[i] / maxTime * columnHeight);
+  renderText(ctx, 
+      Math.floor(times[i]),
+      CLOUD_X + 2 * TEXT_GAP + i * (COLUMN_SPACE + COLUMN_WIDTH),
+      CLOUD_HEIGHT - 4 * GAP - times[i] / maxTime * columnHeight
+  );
   if (names[i] === 'Вы') {
     ctx.fillStyle = getColor(255, 0, 0, 1);
   } else {
     ctx.fillStyle = getColor(getRandomInt(0, 255), getRandomInt(0, 255), 255, 1);
   }
   ctx.fillRect(x, y, width, height);
-  renderText(ctx, names[i], CLOUD_X + 2 * TEXT_GAP + i * (COLUMN_SPACE + COLUMN_WIDTH), CLOUD_HEIGHT - GAP);
+  renderText(ctx,
+      names[i],
+      CLOUD_X + 2 * TEXT_GAP + i * (COLUMN_SPACE + COLUMN_WIDTH),
+      CLOUD_HEIGHT - GAP
+  );
 };
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-  renderText(ctx, 'Ура вы победили!', CLOUD_X + TEXT_GAP, TEXT_GAP * 2);
+  renderText(ctx,
+      'Ура вы победили!',
+      CLOUD_X + TEXT_GAP,
+      TEXT_GAP * 2)
+  ;
   renderText(ctx, 'Список результатов:', CLOUD_X + TEXT_GAP, TEXT_GAP * 3);
   var maxTime = getMaxElement(times);
   for (var i = 0; i < names.length; i++) {
-    renderSingleBar(ctx, CLOUD_X + 2 * TEXT_GAP + i * (COLUMN_SPACE + COLUMN_WIDTH),
+    renderSingleBar(ctx, 
+        CLOUD_X + 2 * TEXT_GAP + i * (COLUMN_SPACE + COLUMN_WIDTH),
         CLOUD_HEIGHT - 3 * GAP - times[i] / maxTime * columnHeight,
-        COLUMN_WIDTH, times[i] / maxTime * columnHeight, names, times, maxTime, i);
+        COLUMN_WIDTH,
+        times[i] / maxTime * columnHeight, 
+        names,
+        times,
+        maxTime,
+        i);
   }
 };
