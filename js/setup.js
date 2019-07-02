0eb8233478f54914723c5b0a785975a6aaa4a9cc
Basay <Basay1980@gmail.com>
2019-07-02 16:09:15 +0300

    сделал функцию для создания обьекта с параметрами для мага, сделал функцию по добавлению магов в фрагмент, удаление класов сделал функцией

--- js/setup.js
+++ js/setup.js
@@ -3,7 +3,6 @@
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnames = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)',
@@ -13,30 +12,30 @@ var coatColors = ['rgb(101, 137, 164)',
   'rgb(215, 210, 55)',
   'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-var wizards = [{
-  name: names[getRandomInt(0, names.length - 1)] + surnames[getRandomInt(0, names.length - 1)],
-  coatColor: coatColors[getRandomInt(0, names.length - 1)],
-  eyesColor: eyesColors[getRandomInt(0, names.length - 1)]
-},
-{
-  name: names[getRandomInt(0, names.length - 1)] + surnames[getRandomInt(0, names.length - 1)],
-  coatColor: coatColors[getRandomInt(0, names.length - 1)],
-  eyesColor: eyesColors[getRandomInt(0, names.length - 1)]
-},
-{
-  name: names[getRandomInt(0, names.length - 1)] + surnames[getRandomInt(0, names.length - 1)],
-  coatColor: coatColors[getRandomInt(0, names.length - 1)],
-  eyesColor: eyesColors[getRandomInt(0, names.length - 1)]
-},
-{
-  name: names[getRandomInt(0, names.length - 1)] + surnames[getRandomInt(0, names.length - 1)],
-  coatColor: coatColors[getRandomInt(0, names.length - 1)],
-  eyesColor: eyesColors[getRandomInt(0, names.length - 1)]
-}];
+var createWizard = function () {
+  var obj = {
+    name: names[getRandomInt(0, names.length - 1)] + surnames[getRandomInt(0, names.length - 1)],
+    coatColor: coatColors[getRandomInt(0, names.length - 1)],
+    eyesColor: eyesColors[getRandomInt(0, names.length - 1)]
+  };
+  return obj;
+};
+var wizards = [createWizard(), createWizard(), createWizard(), createWizard()];
 
-var setupBlock = document.querySelector('.setup');
-setupBlock.classList.remove('hidden');
-document.querySelector('.setup-similar').classList.remove('hidden');
+var removeHidden = function (str) {
+  document.querySelector(str).classList.remove('hidden');
+};
+
+var addWizardsToFragment = function () {
+  for (var i = 0; i < wizards.length; i++) {
+    var wizardElement = templateWizard.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+    fragment.appendChild(wizardElement);
+  }
+};
+removeHidden('.setup');
 
 var templateWizard = document.querySelector('#similar-wizard-template')
 .content.
@@ -44,12 +43,6 @@ querySelector('.setup-similar-item');
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  var wizardElement = templateWizard.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
-  fragment.appendChild(wizardElement);
-}
+addWizardsToFragment();
 similarListElement.appendChild(fragment);
-
+removeHidden('.setup-similar');
